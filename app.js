a527968ba8ac16fc682609d2d1608227168ee283
Karan Wadhwa <wadhwa.karan96@gmail.com>
2017-12-20 01:14:20 +0530

    user model

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+// Mongo connection
+// changed mongoose.connect('blahblahblah') to connection.openUri because of a deprecationWarning
+// check this thread for more info  https://github.com/Automattic/mongoose/issues/5399
+// lol switched it back to connect and added useMongoClient no idea what it does but gets rid of the warning
+mongoose.connect('mongodb://localhost/social', {useMongoClient: true});
+let db = mongoose.connection;
+
+// Check db connection
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Check for db errors
+db.on('error', (err) => {
+  console.log(err);
+});
 
 // Set port
 const port = 3000;
 const app = express();
+
+// Import Models
+let User = require('./models/user');
 
 // Load view engine
 app.set('views', path.join(__dirname, 'views'));
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  reg: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+var User = module.exports = mongoose.model('User', userSchema);
