a86e7bd7db359b94abc3c02a63e0ec1a98b9179b
Karan Wadhwa <wadhwa.karan96@gmail.com>
2017-12-22 23:28:44 +0530

    Forgot Password

--- public/dist/js/index.js
+++ public/dist/js/index.js
@@ -2,3 +2,14 @@
 $(".alert-dismissible").delay(4500).slideUp(500, function(){
   $(".alert-dismissible").alert('close');
 });
+
+function toggleVisibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'inline-block'){
+     e.style.display = 'none';
+     console.log('none');
+  }else {
+     e.style.display = 'inline-block';
+     console.log('block');
+  }
+}
